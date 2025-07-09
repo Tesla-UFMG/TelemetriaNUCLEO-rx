aa4acb5012878692d783ac74db5b341fb704c729
loliveirard <lucas8oliveira@gmail.com>
2025-07-09 09:46:14 -0300

    Add files via upload

---
+++ Core/Src/main.c
@@ -0,0 +1,342 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "subghz.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+#include <string.h>
+#include <stdio.h>
+#include "radio_driver.h"
+#include "stm32wlxx_nucleo.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+#define RF_FREQUENCY                                868000000 /* Hz */
+#define TX_OUTPUT_POWER                             14        /* dBm */
+#define LORA_BANDWIDTH                              0         /* Hz */
+#define LORA_SPREADING_FACTOR                       7
+#define LORA_CODINGRATE                             1
+#define LORA_PREAMBLE_LENGTH                        8         /* Same for Tx and Rx */
+#define LORA_SYMBOL_TIMEOUT                         5         /* Symbols */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+volatile bool rxDone, rxTimeout, rxError;
+uint8_t rxBuf[256];
+uint8_t rxSize;
+char uartBuff[128];
+const char *status;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+void RadioOnDioIrq(RadioIrqMasks_t radioIrq);
+void radioInit(void);
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  char uartBuff[100];
+  int len;
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  // Enable GPIO Clocks
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  // DEBUG_SUBGHZSPI_{NSSOUT, SCKOUT, MSIOOUT, MOSIOUT} pins
+  GPIO_InitStruct.Pin = GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF13_DEBUG_SUBGHZSPI;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  // DEBUG_RF_{HSE32RDY, NRESET} pins
+  GPIO_InitStruct.Pin = GPIO_PIN_10 | GPIO_PIN_11;
+  GPIO_InitStruct.Alternate = GPIO_AF13_DEBUG_RF;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  // DEBUG_RF_{SMPSRDY, LDORDY} pins
+  GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_4;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  // RF_BUSY pin
+  GPIO_InitStruct.Pin = GPIO_PIN_12;
+  GPIO_InitStruct.Alternate = GPIO_AF6_RF_BUSY;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  // RF_{IRQ0, IRQ1, IRQ2} pins
+  GPIO_InitStruct.Pin = GPIO_PIN_3 | GPIO_PIN_5 | GPIO_PIN_8;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_SUBGHZ_Init();
+  MX_USART2_UART_Init();
+  /* USER CODE BEGIN 2 */
+
+  BSP_LED_Init(LED_GREEN);
+  BSP_LED_Init(LED_RED);
+
+  strcpy(uartBuff, "\n\n\n\rTELEMETRIA SLAVE - FORMULA TESLA UFMG\r\nVERSAO=1.0\r\n---------------\r\n");
+  HAL_UART_Transmit(&huart2, (uint8_t *)uartBuff, strlen(uartBuff), HAL_MAX_DELAY);
+  radioInit();
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+
+  SUBGRF_SetDioIrqParams(
+          IRQ_RX_DONE   | IRQ_RX_TX_TIMEOUT | IRQ_CRC_ERROR,
+          IRQ_RX_DONE   | IRQ_RX_TX_TIMEOUT | IRQ_CRC_ERROR,
+          IRQ_RADIO_NONE,
+          IRQ_RADIO_NONE
+  );
+
+  while (1)
+  {
+	rxDone = rxTimeout = rxError = false;
+
+	SUBGRF_SetSwitch(RFO_LP, RFSWITCH_RX);
+	SUBGRF_SetRx(3000 << 6);  // timeout de 3 segundos
+
+	while (!rxDone && !rxTimeout && !rxError);
+
+	if (rxError)    status = "CRC ERR";
+	else if (rxTimeout) status = "TIMEOUT";
+	else             status = "OK";
+
+	len = 0;
+	len += sprintf(uartBuff + len,
+	  "\r\n\r\n"
+	  "Status: %s\r\n", status);
+
+	if (rxDone)
+	{
+
+	  SUBGRF_GetPayload(rxBuf, &rxSize, 0xFF);
+	  len += sprintf(uartBuff + len,
+		  "Payload (%d bytes): ", rxSize);
+	  for (int i = 0; i < 8; i++) {
+		  len += sprintf(uartBuff + len, "%02X ", rxBuf[i]);
+	  }
+	}
+
+	HAL_UART_Transmit(&huart2, (uint8_t*)uartBuff, len, HAL_MAX_DELAY);
+
+	if (rxDone) {
+	  BSP_LED_Toggle(LED_GREEN);
+	}
+
+	HAL_Delay(100);  // pequeno intervalo antes de reiniciar o RX
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+  RCC_OscInitStruct.MSICalibrationValue = RCC_MSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_11;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure the SYSCLKSource, HCLK, PCLK1 and PCLK2 clocks dividers
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK3|RCC_CLOCKTYPE_HCLK
+                              |RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_PCLK1
+                              |RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.AHBCLK3Divider = RCC_SYSCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+
+/**
+  * @brief  Initialize the Sub-GHz radio and dependent hardware.
+  * @retval None
+  */
+void radioInit(void)
+{
+  SUBGRF_Init(RadioOnDioIrq);
+
+  SUBGRF_SetBufferBaseAddress(0x00, 0x00);
+
+  SUBGRF_SetRfFrequency(RF_FREQUENCY);
+  SUBGRF_SetRfTxPower(TX_OUTPUT_POWER);
+
+  SUBGRF_SetPacketType(PACKET_TYPE_LORA);
+
+  ModulationParams_t modParams = {
+    .PacketType = PACKET_TYPE_LORA,
+    .Params.LoRa = {
+      .Bandwidth           = LORA_BW_125,
+      .CodingRate          = LORA_CR_4_5,
+      .SpreadingFactor     = LORA_SF7,
+      .LowDatarateOptimize = 0
+    }
+  };
+  SUBGRF_SetModulationParams(&modParams);
+
+  PacketParams_t pktParams = {
+    .PacketType = PACKET_TYPE_LORA,
+    .Params.LoRa = {
+      .CrcMode        = LORA_CRC_ON,
+      .HeaderType     = LORA_PACKET_VARIABLE_LENGTH,
+      .InvertIQ       = LORA_IQ_NORMAL,
+      .PayloadLength  = 0xFF,          // valor “max” genérico
+      .PreambleLength = LORA_PREAMBLE_LENGTH
+    }
+  };
+  SUBGRF_SetPacketParams(&pktParams);
+}
+
+/**
+  * @brief  Receive data trough SUBGHZSPI peripheral
+  * @param  radioIrq  interrupt pending status information
+  * @retval None
+  */
+void RadioOnDioIrq(RadioIrqMasks_t irq)
+{
+  if (irq == IRQ_RX_DONE)           rxDone    = true;
+  else if (irq == IRQ_RX_TX_TIMEOUT) rxTimeout = true;
+  else if (irq == IRQ_CRC_ERROR)     rxError   = true;
+}
+
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
